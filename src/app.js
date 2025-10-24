5392e465a4a3d7311c670d6972cbe93fae2295de
Sagar Panigrahi <ragh1984fgh@gmail.com>
2025-10-24 14:06:39 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
 import express from "express";
-const app =express()
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-export{app};
+const app = express();
+
+//CORS this file Use for Only to Connect frontend with Backend 
+// so MY frontened can only connect it
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//Express methods are done by use 
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import { app } from "./app.js";
 
 
 dotenv.config({
@@ -7,7 +8,7 @@ dotenv.config({
 })
 connectDB()
 .then(()=>{
-  app.listen(process.env.PORT ||8000 ,()=>
+  app.listen(process.env.PORT || 8000 ,()=>
   console.log("Sucessfull")
 );
 })
